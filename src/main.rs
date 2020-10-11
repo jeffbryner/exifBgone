78c51f7850f1eb437327b0c522b050bff1ce8554
Jeff Bryner <jeff@Jeffs-MacBook-Pro.local>
2020-10-11 12:01:46 -0700

    lil reorg, structure pretty print

--- src/main.rs
+++ src/main.rs
@@ -18,39 +18,47 @@ fn get_target() -> String {
     target.to_string()
 }
 
-fn process_target_file(filename: String) -> Result<String, std::io::Error> {
-    // default
+fn process_file(filename: &str) -> Result<String, std::io::Error> {
     let mut exif = String::from("");
     exif.push_str(&filename);
     exif.push_str(": ");
-    let path = Path::new(&filename);
+    match Metadata::new_from_path(filename.clone()) {
+        Ok(exif_metadata) => {
+            if exif_metadata.has_exif() {
+                exif.push_str("EXIF ");
+            }
+            if exif_metadata.has_iptc() {
+                exif.push_str("IPTC ");
+            }
+            match exif_metadata.get_gps_info() {
+                Some(_) => exif.push_str("GPS "),
+                None => {}
+            };
+        }
+        Err(_) => exif.push_str("error retrieving metadata"),
+    };
+    Ok(exif)
+}
+fn process_target(target: String) -> Result<String, std::io::Error> {
+    // default
+    let mut exif = String::from("");
+    exif.push_str(&target);
+    exif.push_str(": ");
+    let path = Path::new(&target);
     if path.is_dir() {
         for entry in fs::read_dir(path)? {
             //let entry_path = entry?;
             // let athing = entry.path();
             // println!("{:?}", athing);
             if let Ok(entry_path) = entry {
-                println!("{:?}", entry_path.path());
+                println!("{:#?}", entry_path.path());
             }
         }
     }
 
     if path.is_file() {
-        match Metadata::new_from_path(filename.clone()) {
-            Ok(exif_metadata) => {
-                if exif_metadata.has_exif() {
-                    exif.push_str("EXIF");
-                }
-                if exif_metadata.has_iptc() {
-                    exif.push_str("IPTC");
-                }
-                match exif_metadata.get_gps_info() {
-                    Some(_) => exif.push_str("GPS"),
-                    None => {}
-                };
-            }
-            Err(_) => exif.push_str("error retrieving metadata"),
-        };
+        let file_result = process_file(path.to_str().unwrap());
+        println!("{:#?}", file_result);
     }
     Ok(exif)
 }
@@ -59,9 +67,9 @@ fn main() {
     println!("exifBgone: ridding the world of exif tags, one file at a time.");
     let target = get_target();
     println!("targeting: {}", target);
-    let s_result = process_target_file(target);
+    let s_result = process_target(target);
     println!(
-        "result: {:?}",
+        "result: {:#?}",
         s_result.expect("could not retrieve exif data")
     );
 }
